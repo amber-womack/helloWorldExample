90cc8145a679874f1f704fd37535fafa1ba80793
amber-womack <amber.n.womack@gmail.com>
2017-06-07 22:29:46 -0600

    adding hello example file

---
+++ helloWorld.cpp
@@ -0,0 +1,17 @@
+//comadsfjadlfajdf
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int num;
+	cout << "Enter a number: " << endl;
+	cin >> num;
+
+	cout << num << endl;
+	cout << "hello World!!!!!" << endl;
+
+	return 0;
+}
